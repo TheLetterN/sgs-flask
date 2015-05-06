fc214a42dbe2a443fa105d7b15d9e5bd632bf2cf
TheLetterN <n@fluffyware.com>
2015-05-06 13:38:22 -0700

    Added test command to manage.py to run all unit tests in tests.py

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 #!venv/bin/python
 """Manage running, testing, and interacting with sgs_flask.
 Use manage.py --help to list available commands."""
+import unittest
 from flask.ext.script import Manager, Command
 from app import app
+import tests
 
 #Create a Manager object to assign commands to
 manager = Manager(app)
@@ -12,6 +14,13 @@ manager = Manager(app)
 def hello():
     """Say hello to your little friend."""
     print "Why are you talking to a computer program?"
+
+@manager.command
+def test():
+    """Run all unit tests from tests.py."""
+    suite = unittest.TestLoader().loadTestsFromModule(tests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
 
 if __name__ == '__main__':
     manager.run()

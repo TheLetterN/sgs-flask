8e5b70fcb34ce1a70f91008e45d6030f907efc36
TheLetterN <n@fluffyware.com>
2015-06-03 17:32:43 -0700

    Switched from using unittest to using nose.

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!venv/bin/python
 """Manage running, testing, and interacting with sgs_flask.
 Use manage.py --help to list available commands."""
-import unittest
+import nose
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from app import app
@@ -20,12 +20,14 @@ def hello():
     """Say hello to your little friend."""
     print "Why are you talking to a computer program?"
 
+#@manager.command
+#def test():
+#    """Run all unit tests from tests.py."""
+#    suite = unittest.TestLoader().discover('tests')
+#    unittest.TextTestRunner(verbosity=2).run(suite)
 @manager.command
 def test():
-    """Run all unit tests from tests.py."""
-    suite = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(suite)
-
+    nose.main(argv=['', '--verbosity=3'])
 
 #Manage our database and migrations
 manager.add_command('db', MigrateCommand)

89e8a4d22c5e3f0a6b139da62e470cd1808489e9
TheLetterN <n@fluffyware.com>
2015-06-05 12:18:20 -0700

    I've moved everything to a /old folder, and will be re-making the app from the ground up. There are several (hopefully) good reasons for this: 1) The existing project does not make use of Flask blueprints, and it would be difficult to refactor it to do so. 2) The existing test suite is not modular, and full of dirty hybrid tests. I need to ask myself when writing tests if I'm really testing what I should be testing. 3) The database schema is going to undergo some extremely big changes, so it will be easier to re-do it than to refactor what we already have. 4) I have a much more concrete idea of howthe project should be structured now, and it will be easier to manage if I do it that way from the get-go.

--- old/manage.py
+++ old/manage.py
@@ -1,36 +1,33 @@
 #!venv/bin/python
 """Manage running, testing, and interacting with sgs_flask.
 Use manage.py --help to list available commands."""
-import nose
+import pytest
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from app import app
 from app.models import db
-import tests
 
-#Create a Manager object to assign commands to
+# Create a Manager object to assign commands to
 manager = Manager(app)
 
-#Create a migrate object
+# Create a migrate object
 migrate = Migrate(app, db)
 
-#Example of creating a command using the manager.command decorator
+
+# Example of creating a command using the manager.command decorator
 @manager.command
 def hello():
     """Say hello to your little friend."""
     print "Why are you talking to a computer program?"
 
-#@manager.command
-#def test():
-#    """Run all unit tests from tests.py."""
-#    suite = unittest.TestLoader().discover('tests')
-#    unittest.TextTestRunner(verbosity=2).run(suite)
+
 @manager.command
 def test():
-    nose.main(argv=['', '--verbosity=3'])
+    pytest.main('tests')
 
-#Manage our database and migrations
+
+# Manage our database and migrations
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+        manager.run()

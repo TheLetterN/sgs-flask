232c795cc77e1c6ea15da21812cb99a2d5a5577b
N <n@fluffyware.org>
2015-06-10 16:56:49 -0700

    Added the beginnings of our config.py file.

---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+from tempfile import gettempdir
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+tempdir = gettempdir()
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SGS_SECRET_KEY') or \
+        '\xbdc@:b\xac\xfa\xfa\xd1z[\xa3=\xd1\x9a\x0b&\xe3\x1d5\xe9\x84(\xda'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    ADMINISTRATORS = os.environ.get('SGS_ADMINISTRATORS')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SGS_DEV_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'development.db')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SGS_TEST_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(tempdir, 'testing.db')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SGS_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'sgs.db')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}

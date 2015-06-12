cdcc9fb7c69b881dad9cd8be72d7845d1d72809b
N <n@fluffyware.org>
2015-06-12 14:39:25 -0700

    Added our first unit tests with test_app.py. They just test that an app created in the test script exists and is in testing mode right now.

--- manage.py
+++ manage.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 import os
+import pytest
 from flask.ext.script import Manager
 from app import create_app
 
 app = create_app(os.getenv('SGS_CONFIG') or 'default')
 manager = Manager(app)
+
+
+@manager.command
+def hello():
+    """Say hello to your Flask site."""
+    print('Why are you talking to a python script?')
+
+@manager.command
+def test():
+    """Run tests."""
+    pytest.main('tests')
+
 
 if __name__ == '__main__':
     manager.run()
---
+++ tests/test_app.py
@@ -0,0 +1,25 @@
+"""
+    Tests basic functionality of our Flask application.
+"""
+
+import unittest
+from flask import current_app
+from app import create_app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+    def test_is_testing(self):
+        self.assertTrue(current_app.config['TESTING'])
+
+if __name__ == '__main__':
+    unittest.main()

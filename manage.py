ee771fe0a4557a94cf6060a5c2f384d8e8013f82
N <n@fluffyware.org>
2015-06-12 15:57:54 -0700

    Added directory and some blank files for our auth module, and added flask-migrate support.

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import os
 import pytest
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager, Shell
-from app import create_app
+from app import create_app, db
 
 app = create_app(os.getenv('SGS_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
@@ -23,8 +25,8 @@ def test():
     """Run tests."""
     pytest.main('tests')
 
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()

50d2bf547d3ed954002c99020f822efa57e6a994
N <n@fluffyware.org>
2015-06-12 14:00:51 -0700

    Copied the old main.css file to app/static/css/ and made the first Jinja templates for our main module, and changed app/main/views.py to render the main/index.html template. Tested it, and so far so good!

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from . import main
 
 
 @main.route('/')
 def index():
-    return 'Test page please ignore.'   # TEMP
+    return render_template('main/index.html')

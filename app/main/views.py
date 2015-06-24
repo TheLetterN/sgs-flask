a8d9c832d332da140e958c3c8696c58480b75246
N <n@fluffyware.org>
2015-06-24 16:15:33 -0700

    Replaced the <head> contents of app/templates/main/base.html with a Jinja block so we can easily have different <head> contents on each page. Edited app/templates/main/index.html to include the new head block, and added a canonical link to the main index route ('/') just in case, though I also implemented routes for '/index' and '/index.html' as 301 redirects instead of stacking them onto the main index() function, and a unit test to make sure those pages return 301 status codes. Also edited app/templates/auth/login.html to add a head block, and renamed the content block from app/templates/base.html to 'body', and edited templates that extend it to reflect that change.

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import redirect, render_template, url_for
 from . import main
 
 
 @main.route('/')
 def index():
     return render_template('main/index.html')
+
+
+@main.route('/index')
+@main.route('/index.html')
+def index_redirect():
+    """For convenience, we want common index pages to be redirected to /"""
+    return redirect(url_for('main.index'), code=301)
--- tests/test_main_views.py
+++ tests/test_main_views.py
@@ -18,6 +18,13 @@ class TestMainRoutes(unittest.TestCase):
         retval = self.tc.get('/')
         self.assertEqual(retval.status_code, 200)
 
+    def test_other_index_pages_redirect_to_main_index(self):
+        """/index and /index.html should give 301 redirect responses."""
+        retval1 = self.tc.get('/index', follow_redirects=False)
+        retval2 = self.tc.get('/index.html', follow_redirects=False)
+        self.assertEqual(retval1.status_code, 301)
+        self.assertEqual(retval2.status_code, 301)
+
 
 if __name__ == '__main__':
     unittest.main()

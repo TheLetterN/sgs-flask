86bd091b7b0cb95391d60fcaab8a585ff0174c87
N <n@fluffyware.org>
2015-06-25 17:05:44 -0700

    Finally figured out how to add options in flask-script without the need for additional arguments, which allowed me to add a -f and --fast option which run the tests in tests/fast_tests instead of all tests.

--- manage.py
+++ manage.py
@@ -14,16 +14,30 @@ def make_shell_context():
     return dict(app=app, db=db)
 
 
-@manager.command
-def hello():
+@manager.option(
+    '-g',
+    '--goodbye',
+    action='store_true',
+    help="I heard a song like this once.")
+def hello(goodbye=False):
     """Say hello to your Flask site."""
-    print('Why are you talking to a python script?')
+    if goodbye:
+        print('I don\'t know why you say goodbye, I say hello!')
+    else:
+        print('Why are you talking to a python script?')
 
 
-@manager.command
-def test():
-    """Run tests."""
-    pytest.main('tests')
+@manager.option(
+    '-f',
+    '--fast',
+    action='store_true',
+    help="Do not run database tests.")
+def test(fast=False):
+    """Run all tests. -f or --fast to skip slow tests that use the database."""
+    if fast:
+        pytest.main('tests/fast_tests')
+    else:
+        pytest.main('tests')
 
 manager.add_command('db', MigrateCommand)
 manager.add_command('shell', Shell(make_context=make_shell_context))

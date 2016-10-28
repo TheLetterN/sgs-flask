3164e777acb3421eed8e8ad5607cc5185316ca63
N <thisissomethingthathappens@gmail.com>
2016-10-27 17:03:45 -0700

    Finally isolated all (I hope) needed data. Yay!

--- sgsscrape.py
+++ sgsscrape.py
@@ -1,16 +1,20 @@
 from bs4 import BeautifulSoup, Comment
+from inflection import pluralize
 import requests
 
 from app.db_helpers import dbify
 
 
 def first_text(tag):
-    return next(
-        (c for c in tag.contents if isinstance(c, str) and
-            not isinstance(c, Comment) and
-            c.strip()),
-        ''
-    ).strip()
+    try:
+        return next(
+            (c for c in tag.contents if isinstance(c, str) and
+                not isinstance(c, Comment) and
+                c.strip()),
+            ''
+        ).strip()
+    except AttributeError:
+        return ''
 
 
 def extract_comments(tag):
@@ -20,6 +24,14 @@ def extract_comments(tag):
             recursive=False
         )
     )
+
+
+def orblank(var):
+    return var if var else ''
+
+
+def tags_to_str(tags):
+    return '\n'.join(str(t) for t in tags)
 
 
 def get_subsections(tag):
@@ -58,10 +70,13 @@ def parse_button(button):
     }
 
 
+def clean_title(title):
+    return dbify(title.lower().replace('seeds', '').strip())
+
+
 class CultivarTag:
     def __init__(self, tag):
         self.tag = tag
-        self.d = dict()
         self.images = tag.find_all('img')
         self.new_for = tag.find('span', class_='Cultivar_span_new')
         self.favorite = tag.find('span', class_='Cultivar_span_best_seller')
@@ -85,16 +100,35 @@ class CultivarTag:
                 self.bn_em = em
         self.ps = tag.find_all('p')
         self.buttons = tag.find_all('button')
-        self.populate_d()
-
-    def populate_d(self):
-        self.d['name'] = first_text(self.h3)
-        self.d['h3 ems'] = [first_text(em) for em in self.h3_ems]
-        self.d['ps'] = [str(p) for p in self.ps]
-        self.d['buttons'] = [parse_button(b) for b in self.buttons]
+        self._dbdict = dict()
 
     def __repr__(self):
         return '<CultivarTag "{}">'.format(self.d['name'])
+
+    def get_packet_dicts(self):
+        return [
+            {
+                'sku': b['data-item-id'],
+                'amount': b['data-item-description'],
+                'product_name': b['data-item-name'],
+                'price': b['data-item-price'],
+                'taxable': True if 't' in b['data-item-taxable'] else False
+            } for b in self.buttons
+        ]
+
+    @property
+    def dbdict(self):
+        if not self._dbdict:
+            print('Creating dbdict for cultivar "{}"...'.format(self.h3))
+            self._dbdict['name'] = first_text(self.h3)
+            self._dbdict['subtitle'] = first_text(self.subtitle_em)
+            self._dbdict['botanical_names'] = first_text(self.bn_em)
+            self._dbdict['description'] = tags_to_str(self.ps)
+            self._dbdict['new_for'] = '2017' if self.new_for else ''
+            self._dbdict['favorite'] = True if self.favorite else False
+            self._dbdict['images'] = [i['src'] for i in self.images]
+            self._dbdict['packets'] = self.get_packet_dicts()
+        return self._dbdict
 
 
 class SectionTag:
@@ -106,12 +140,42 @@ class SectionTag:
             class_=lambda x: x and x.lower() == 'section' or
             x.lower() == 'series'
         )
-        self.intro = self.header.find_all('p') if self.header else None
+        self.header_h2 = self.header.find('h2')
+        self.header_ems = self.header_h2.find_all('em')
+        if len(self.header_ems) == 1:
+            self.subtitle = self.header_h2.find(
+                'em',
+                class_=lambda x: x and 'Series_em' in x
+            )
+            self.botanical_names = self.header_h2.find(
+                'em',
+                class_=lambda x: x and 'Section_em' in x
+            )
+        elif len(self.header_ems) > 1:
+            self.subtitle = self.header_ems[0]
+            self.botanical_names = self.header_ems[1]
+        else:
+            self.subtitle = None
+            self.botanical_names = None
+        self.intro = self.header.find_all('p') if self.header else ''
         self.cultivars = get_cultivars(tag)
         self.comments = extract_comments(tag)
+        self._dbdict = dict()
 
     def __repr__(self):
         return '<SectionTag class="{}">'.format(self.class_)
+
+    @property
+    def dbdict(self):
+        if not self._dbdict:
+            print('Creating dbdict for section "{}"...'.format(self.header_h2))
+            self._dbdict['name'] = first_text(self.header_h2)
+            self._dbdict['subtitle'] = first_text(self.subtitle)
+            self._dbdict['botanical_names'] = first_text(self.botanical_names)
+            self._dbdict['description'] = tags_to_str(self.intro)
+            self._dbdict['subsections'] = [s.dbdict for s in self.subsections]
+            self._dbdict['cultivars'] = [c.dbdict for c in self.cultivars]
+        return self._dbdict
 
     @property
     def class_(self):
@@ -142,28 +206,55 @@ class IndexScraper:
         self.main = self.soup.find('div', id='main')
         if not self.main:
             raise RuntimeError('No main div on index page: {}'.format(url))
+        self.h1 = self.main.find('h1')
         self.links = self.main.find(
             'div',
             class_='Index_pages-image-links-wrapper'
         ).find_all('a')
         self.link_urls = [a['href'] for a in self.links]
-        self._pages = None
+        self.intro = self.main.find_all('p', class_='Index_pages-intro')
+        self.intro += self.main.find_all(
+            'blockquote',
+            class_='Index_pages-blockquote'
+        )
+        self.intro += self.main.find_all(
+            'i',
+            class_='Index_pages-blockquote-i'
+        )
+        self._common_names = None
+        self._dbdict = dict()
 
     @property
-    def pages(self):
-        if not self._pages:
-            print('Scraping pages...')
-            self._pages = []
-            for u in self.link_urls:
-                print('Scraping {}...'.format(u))
-                self._pages.append(CNScraper(u))
-        return self._pages
+    def common_names(self):
+        if not self._common_names:
+            print('Scraping common name pages...')
+            self._common_names = []
+            for l in self.links:
+                url = l['href']
+                thumb = l.find('img')['src']
+                print('Scraping {}...'.format(url))
+                self._common_names.append(CNScraper(url, thumb))
+        return self._common_names
+
+    @property
+    def dbdict(self):
+        if not self._dbdict:
+            print('Creating dbdict for "{}"...'.format(self.url))
+            name = clean_title(first_text(self.h1))
+            self._dbdict['name'] = name
+            self._dbdict['slug'] = pluralize(name.split(' ')[0].lower())
+            self._dbdict['description'] = tags_to_str(self.intro)
+            self._dbdict['common_names'] = [
+                c.dbdict for c in self.common_names
+            ]
+        return self._dbdict
 
 
 class CNScraper:
     """A scraper for a given common name page."""
-    def __init__(self, url):
+    def __init__(self, url, thumbnail=None):
         self.url = url
+        self.thumbnail = thumbnail
         r = requests.get(url)
         r.encoding = 'utf-8'
         self.soup = BeautifulSoup(r.text, 'html5lib')
@@ -173,6 +264,9 @@ class CNScraper:
         self.sections = get_subsections(self.main)
         self.cultivars = get_cultivars(self.main)
         self.header = self.main.find('div', class_='Header')
+        self.header_h1 = self.header.find('h1')
+        self.header_h2 = self.header.find('h2')
+        self.header_h3 = self.header.find('h3')
         self.sun = self.header.find(
             'p',
             class_=lambda x: x and 'sun' in x or 'shade' in x
@@ -185,10 +279,42 @@ class CNScraper:
         self.related_links = None
         self.parse_related_and_nav()
         self.consolidate_cultivars()
-
+        self._dbdict = dict()
 
     def __repr__(self):
         return '<CNScraper url="{}">'.format(self.url)
+
+    @property
+    def dbdict(self):
+        if not self._dbdict:
+            print('Creating dbdict for "{}"...'.format(self.url))
+            self._dbdict['name'] = clean_title(first_text(self.header_h1))
+            try:
+                self._dbdict['description'] = tags_to_str(self.intro.contents)
+            except AttributeError:
+                self._dbdict['description'] = ''
+            self._dbdict['subtitle'] = first_text(self.header_h2)
+            self._dbdict['botanical_names'] = first_text(self.header_h3)
+            self._dbdict['slug'] = self.url.split('/')[-1].replace('.html', '')
+            self._dbdict['sunlight'] = next(
+                c for c in self.sun['class'] if 'sun' in c or 'shade' in c
+            )
+            self._dbdict['thumb_url'] = self.thumbnail
+            try:
+                self._dbdict['instructions'] = tags_to_str(
+                    self.growing.contents
+                )
+            except AttributeError:
+                self._dbdict['instructions'] = ''
+            try:
+                self._dbdict['related_links'] = [
+                    a['href'] for a in self.related_links
+                ]
+            except TypeError:
+                self._dbdict['related_links'] = []
+            self._dbdict['sections'] = [s.dbdict for s in self.sections]
+            self._dbdict['cultivars'] = [c.dbdict for c in self.cultivars]
+        return self._dbdict
 
     def parse_related_and_nav(self):
         rls = self.main.find_all('div', class_='RelatedLinks')
